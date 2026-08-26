95b2ae0037b709387a4399101c97e0f4ea23ddc3
Nghia (Randy) Ha <69777686+Nghiahadev@users.noreply.github.com>
2026-08-26 12:06:21 -0400

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,11 +7,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 1,
     },
     {
       url: `${baseUrl}/projects`,
       lastModified: new Date(),
+      changeFrequency: "monthly",
       priority: 0.9,
     },
   ];
